fcbd0a6c922e4c047f125c10eb71742396df40ae
peter <PeterRong96@gmail.com>
2017-05-22 19:34:21 +0800

    sandbox/tracer/src/lib.rs: framework half done.
    Signed-off-by: peter <PeterRong96@gmail.com>

--- tracer/src/lib.rs
+++ tracer/src/lib.rs
@@ -1,31 +1,51 @@
 extern crate libc;
 
+#[derive(Debug)]
+pub struct Registers {
+	field: Type
+}
+
 pub enum Request{
 	TraceMe = 0,
-	
+
 
 }
 
-// perform the base request
-pub fn base_request(option: Request, pid: i32, addr: &mut i8, data: i32) -> Result<i64, i64>{
-	let res;
-	unsafe{
-		res = libc::ptrace(option as u32, pid, addr, data);
+#[derive(Debug, Clone)]
+pub struct Tracee {
+	pid: libc::pid_t,
+}
+
+impl Tracee {
+	pub fn new(pid: i32) -> Result<Tracee>{
+		unimplemented!();
+	}
+	pub fn new(args: &Vec<String>) -> Result<Tracee>{
+		unimplemented!();	
+	}
+	// perform the base request
+	pub fn base_request(&self, option: Request, addr: &mut i32, data: i32) -> Result<i64, i64>{
+		let res;
+		unsafe{
+			res = libc::ptrace(option as u32, self.pid, addr, data);
+		}
+
+		// error handling, TODO peek user need special care
+		match res {
+			-1 => Err(-1),
+			_  => Ok(res),
+		}
 	}
 
-	// error handling, TODO peek user need special care
-	match res {
-		-1 => Err(-1),
-		_  => Ok(res),
+	// indicate the current process should be traced by its parent
+	pub fn trace_me(&self) -> Result<i64,i64> {
+		// the pid, addr and data will be ignored
+		let mut temp = 0;
+		self.base_request(Request::TraceMe, &mut temp, 0)
 	}
 }
 
-// indicate the current process should be traced by its parent
-pub fn ptrace_traceme() -> Result<i64,i64> {
-	// the pid, addr and data will be ignored
-	let mut temp = 0;
-	base_request(Request::PtraceTraceme, 0, &mut temp, 0)
-}
+
 
 
 
