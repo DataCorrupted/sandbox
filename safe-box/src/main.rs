8d5e77f32b3eba2fc761351c553f2cbd0998eecf
jianxiong Cai <caijx@shanghaitech.edu.cn>
2017-05-27 13:14:20 +0800

    safe-box: main.rs update framework -- this is an unstable version
    Signed-off-by: jianxiong Cai <caijx@shanghaitech.edu.cn>

--- safe-box/src/main.rs
+++ safe-box/src/main.rs
@@ -1,18 +1,34 @@
 extern crate tracer;
+extern crate libc;
 use std::env;
 
+// retrun when one of the child called syscall
+fn safe_wait(){
+	let mut status = 0;
+	unsafe {
+		libc::wait(&mut status);
+	}
+}
 
 fn main() {
 	// read arguments from argvs and turn it into a vec
 	let mut argvs = Vec::new();
-	let mut argvs_raw = env::args();
+	let argvs_raw = env::args();
 	for x in argvs_raw{
 		argvs.push(x);
 	}
 
 	// create a new tracee
+	let tracee = tracer::Tracee::new(&argvs).unwrap();
 
 	// wait for execvp and start the tracee
+	
+	// test only
+	loop {
+		safe_wait();
+		tracee.do_continue();
+	}
+
 
 	// the following things is in a while loop
 	// wait for every sys call the tracee make and then determine whether the syscall is valid 
