4a2a98b8b5835229141802fac7f0551f5b0962ce
Jianxiong Cai <caijx@shanghaitech.edu.cn>
2017-05-29 17:10:49 +0800

    safe-box: Add two file, enable configure file
    file_conf.rs: The helper class to control the file premission
    file_permission.conf: The config file for fiel premission
    
    Signed-off-by: Jianxiong Cai <caijx@shanghaitech.edu.cn>

---
+++ safe-box/src/file_conf.rs
@@ -0,0 +1,48 @@
+use std::fs::File;
+use std::io::BufReader;
+use std::io::BufRead;
+use std::cmp::Ordering;
+
+pub struct FileConf{
+	premitted_files: Vec<String>
+}
+
+impl FileConf {
+	pub fn new() -> FileConf{
+		let mut conf = FileConf {premitted_files: Vec::new(),};
+
+		// read the file line by line
+		// open the file
+		let f_res = File::open("file_permission.conf");
+		match f_res {
+			Ok(fd) => {
+				// create reader
+				let reader = BufReader::new(&fd);
+				// read the file 
+				for x in reader.lines(){
+					let l = x.unwrap();
+					// if the line is not a commment
+					if (l.len() > 0) && (l.as_bytes()[0] != '#' as u8){
+						conf.premitted_files.push(String::from(l));
+					}
+				};
+				conf
+			},
+			_ => conf,
+		}
+
+	}
+
+	pub fn is_file_premitted(&self, que: &String) -> bool{
+		for x in self.get_files(){
+			if x.partial_cmp(que) == Some(Ordering::Equal){
+				return true;
+			}
+		}
+		return false;
+	}
+
+	fn get_files(&self) -> Vec<String>{
+		self.premitted_files.clone()
+	}
+}
