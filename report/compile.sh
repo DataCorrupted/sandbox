33e7b8c8e6a48d6d178e3282478eba981b677a1c
peter <PeterRong96@gmail.com>
2017-06-11 14:54:26 +0800

    report framework finished.
    Signed-off-by: peter <PeterRong96@gmail.com>

---
+++ report/compile.sh
@@ -0,0 +1,9 @@
+latex OS_report.tex
+bibtex OS_report.aux
+latex OS_report.tex
+latex OS_report.tex
+rm *.bbl *.blg *.log *.aux
+dvips OS_report.dvi
+ps2pdf OS_report.ps
+rm *.dvi *.ps
+mv OS_report.pdf ../
